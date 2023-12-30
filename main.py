169879030c75968681654a27e3770ba2b326ee04
AliBadran716 <102072821+AliBadran716@users.noreply.github.com>
2023-12-30 20:14:56 +0200

    database

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtGui import QPixmap
 from PyQt5.uic import loadUiType
 import pyqtgraph as pg
-import cv2
+# import cv2
 import numpy as np
 import pandas as pd
 import os
@@ -15,13 +15,14 @@ from os import path
 import wave
 import threading
 import pyaudio
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget,QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog
 from recording import AudioRecorder
 from scipy.signal import spectrogram
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib import pyplot as plt
 from voice import Voice
+
 # from matplotlib import MatplotlibWidget
 
 
@@ -36,19 +37,35 @@ class MainApp(QMainWindow, FORM_CLASS):
         self.setupUi(self)
         self.handle_button()
         self.passwords = {
-            "unlock_the_gate": ["unlock_the_gate.wav"    #wav file
-                                , None                   #similarity factor
-                                , None],                 #instance
-            "open_middle_door": ["open_middle_door.wav" 
-                                 , None 
-                                 , None   ],
-            "grant_me_access": ["grant_me_access.wav" 
-                                , None 
-                                , None   ],
+            "unlock_the_gate": [
+                ["voice_data_base/ahmed_ali_unlock_the_gate.wav", "voice_data_base/bedro_unlock_the_gate.wav"]
+                # list of wav files stored in voice_data_base folder
+                , None  # similarity factor
+                , [None, None]  # instance
+            ],
+            "open_middle_door": [
+                ["voice_data_base/ahmed_ali_open_middle_door.wav", "voice_data_base/bedro_open_middle_door.wav"]
+                # list of wav files stored in voice_data_base folder
+                , None  # similarity factor
+                , [None, None]  # instance
+            ],
+            "grant_me_access": [
+                ["voice_data_base/ahmed_ali_grant_me_access.wav", "voice_data_base/bedro_grant_me_access.wav"]
+                # list of wav files stored in voice_data_base folder
+                , None  # similarity factor
+                , [None, None]  # instance
+            ],
         }
+
+        self.create_password_audio_instance()  # create instance of each password
 
     def handle_button(self):
         self.pushButton.clicked.connect(self.start_recording)
+
+    def create_password_audio_instance(self):
+        for password in (self.passwords.keys()):
+            for i in range(len(self.passwords[password][0])):
+                self.passwords[password][2][i] = Voice(self.passwords[password][0][i])
 
     def start_recording(self):
         print("Start Recording")
@@ -59,12 +76,10 @@ class MainApp(QMainWindow, FORM_CLASS):
         else:
             self.pushButton.setText("Start")
             self.recorder.stop_recording()
-            self.spectrogram(self.recorder.frames,44100,self.widget)
-            self.spectrogram(self.recorder.frames,44100,self.widget)
+            self.spectrogram(self.recorder.frames, 44100, self.widget)
             self.recorder.save_audio('trial.wav')
             test_voice = Voice('trial.wav')
-            for password in self.passwords:
-                self.passwords[password][2] = Voice(self.passwords[password][0])    
+
             for password in self.passwords:
                 self.passwords[password][1] = self.check_similarity(test_voice, self.passwords[password][2])
             max = 0
@@ -75,41 +90,37 @@ class MainApp(QMainWindow, FORM_CLASS):
 
             self.label.setText(max_password)
 
-
-
-    
     def check_similarity(self, test_voice, password_voice):
         features1 = test_voice.get_stft()
-        features2 = password_voice.get_stft()
-        features1, features2 = self.match_signal_length(features1, features2)
-        
-        # similarity_score = np.linalg.norm(features1 - features2)
-        similarity_score = np.dot(features1.T, features2) / (np.linalg.norm(features1) * np.linalg.norm(features2))
+        similarity_score = []
+        for i in range(len(password_voice)):
+            features2 = password_voice[i].get_stft()
+            features1, features2 = self.match_signal_length(features1, features2)
+
+            # similarity_score = np.linalg.norm(features1 - features2)
+            similarity_score.append(np.mean(np.dot(features1.T, features2) / (np.linalg.norm(features1) * np.linalg.norm(features2))))
+        similarity_score = np.max(similarity_score)
         print(similarity_score)
-        similarity_score = np.mean(similarity_score)
         return similarity_score
-    
+
     def match_signal_length(self, signal1, signal2):
         max_length = max(len(signal1), len(signal2))
         signal1 = np.pad(signal1, (0, max_length - len(signal1)), 'constant')
         signal2 = np.pad(signal2, (0, max_length - len(signal2)), 'constant')
         return signal1, signal2
-    
 
     def spectrogram(self, data, sampling_rate, widget):
         if widget.layout() is not None:
-            
             widget.layout().deleteLater()
-        
-        print("Spectrogram")
+
         data = np.frombuffer(b''.join(data), dtype=np.int16)
-        print(data)
+        # print(data)
         fig = Figure()
-        fig = Figure(figsize=(2,3))
+        fig = Figure(figsize=(2, 3))
         ax = fig.add_subplot(111)
         Sxx = ax.specgram(data, Fs=sampling_rate, cmap='plasma')
         # time_axis = np.linspace(0, len(data) / sampling_rate, num=Sxx.shape[1])
-        
+
         # ax.imshow(10 * np.log10(Sxx), aspect='auto', cmap='viridis',extent=[time_axis[0], time_axis[-1], 0, sampling_rate / 2])
         ax.axes.plot()
         canvas = FigureCanvas(fig)
@@ -117,6 +128,7 @@ class MainApp(QMainWindow, FORM_CLASS):
         layout.addWidget(canvas)
         widget.setLayout(layout)
         widget.show()
+
 
 def main():  # method to start app
     app = QApplication(sys.argv)
